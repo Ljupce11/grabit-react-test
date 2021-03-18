68c92cefea3173cbc309e6c9d5fbdbc80ed9858f
Ljubomir Bojadziev <ljubomir.bojadziev@keitaro.com>
2021-03-18 13:46:03 +0100

    Create helpers that calculate total bucket storage size and returns readable file size string

---
+++ src/helpers/calculateBucketStorageSize.ts
@@ -0,0 +1,11 @@
+import { BucketObject } from "../interfaces/interfaces"
+
+export const calculateBucketStorageSize = (bucketObjects: BucketObject[]) => {
+  let totalStorageSize = 0
+  if (bucketObjects && bucketObjects.length > 0) {
+    bucketObjects.forEach(object => {
+      totalStorageSize += Number(object.size)
+    })
+  }
+  return totalStorageSize
+}
---
+++ src/helpers/getReadableFileSizeString.ts
@@ -0,0 +1,11 @@
+export const getReadableFileSizeString = (fileSizeInBytes: number) => {
+  let i = -1;
+  const byteUnits = [' KB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  do {
+    fileSizeInBytes = fileSizeInBytes / 1024;
+    i++;
+  } while (fileSizeInBytes > 1024);
+
+  return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+};
