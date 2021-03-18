371960b4411e84e7115713703ae911d0a2e61188
Ljubomir Bojadziev <ljubomir.bojadziev@keitaro.com>
2021-03-18 14:14:51 +0100

    Implement context state for errorMessaage and routes for Dashboard and BucketDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 
+import { ErrorMessageContext } from './context/ErrorMessageContext';
+import { Navbar } from './components/shared/Navbar/Navbar';
+import { ErrorModal } from './components/shared/ErrorModal/ErrorModal';
+import { Dashboard } from './components/main/Dashboard/Dashboard';
+import { BucketDetails } from './components/main/BucketDetails/BucketDetails';
+
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        {
+          errorMessage &&
+          <ErrorModal
+            errorMessage={errorMessage}
+            show={errorMessage ? true : false}
+            onHide={() => setErrorMessage(null)} />
+        }
+        <Switch>
+          <ErrorMessageContext.Provider value={{ errorMessage, setErrorMessage }}>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/buckets/:bucketId" component={BucketDetails} />
+          </ErrorMessageContext.Provider>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
