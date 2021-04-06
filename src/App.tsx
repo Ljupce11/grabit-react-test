8754b7eab94bf0f02fd6590cf8f248d19bc2bc44
Ljubomir Bojadziev <ljubomir.bojadziev@keitaro.com>
2021-04-06 13:23:36 +0200

    Install scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import './App.css';
+import './App.scss';
 
 import { ErrorMessageContext } from './context/ErrorMessageContext';
 import { Navbar } from './components/shared/Navbar/Navbar';
